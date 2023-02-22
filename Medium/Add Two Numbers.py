07c12234204fdfe351df19c4398aa4f3d44881d9
TrigrD3 <arya66151@gmail.com>
2023-02-22 19:04:59 +0800

    add comments to Add Two Numbers.py

--- Medium/Add Two Numbers.py
+++ Medium/Add Two Numbers.py
@@ -7,21 +7,35 @@ class ListNode:
         self.next = next
 
 class Solution:
+    # Function to add two linked lists
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # Create a dummy node to store the sum of the two linked lists
         dummyHead = ListNode(0)
+        # Create a pointer to the dummy node
         curr = dummyHead
+        # Initialize carry to 0
         carry = 0
+        # Loop through both linked lists until we reach the end of both lists and there is no carry left
         while l1 != None or l2 != None or carry != 0:
+            # Get the value of l1 and l2 (or 0 if they are None)
             l1Val = l1.val if l1 else 0
             l2Val = l2.val if l2 else 0
+            # Add the values of l1, l2, and carry
             columnSum = l1Val + l2Val + carry
+            # Update carry based on the column sum
             carry = columnSum // 10
+            # Create a new node to store the ones digit of the column sum
             newNode = ListNode(columnSum % 10)
+            # Add the new node to the end of the result linked list
             curr.next = newNode
+            # Move the pointer to the new node
             curr = newNode
+            # Move the pointers for l1 and l2 to the next nodes (or None if there are no more nodes)
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
+        # Return the result linked list (skipping the dummy node)
         return dummyHead.next
+
     
 
 
